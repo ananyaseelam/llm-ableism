63afcc84a58df5575081e77f6320b0e3f8506155
Ananya Seelam <as2759@cornell.edu>
2024-02-08 01:13:23 -0500

    cleaned up the code and added some comments

--- perspective-api.py
+++ perspective-api.py
@@ -11,6 +11,7 @@ client = discovery.build(
   static_discovery=False,
 )
 
+# A function that takes as input a sentence prompt and returns an object with toxicity data
 def toxicity_rating(prompt):
   analyze_request = {
     'comment': { 'text': prompt },
@@ -20,15 +21,19 @@ def toxicity_rating(prompt):
   response = client.comments().analyze(body=analyze_request).execute()
   return json.dumps(response, indent=2)
 
-f = open("prompts.txt", "r")
-output = open("toxicity", "a")
+# A function that obtains toxicity for a list of sentences in a .txt file and 
+# puts the data in a file called toxicity.txt.  
+def retrieve_toxicity_data(filename):
+  f = open(filename, "r")
+  output = open("toxicity.txt", "a")
 
-for sentence in f:
-  response = toxicity_rating(sentence)
-  output.write(sentence)
-  output.write(response)
-output.close()
-f.close()
-# data = toxicity_rating('friendly greetings from python')
-# print(data)
-# print(data[attributeScores])
+  for sentence in f:
+    print(sentence)
+    response = toxicity_rating(sentence)
+    output.write(sentence)
+    output.write(response)
+  output.close()
+  f.close()
+
+retrieve_toxicity_data("prompts.txt")
+
