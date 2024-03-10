e5e8d2def84e7e462146ce77e9a5d9da0d0208ef
Ananya Seelam <as2759@cornell.edu>
2024-03-10 18:13:10 -0400

    refactored code for perspective api data collection and collected data for new set of prompts

---
+++ perspective-api/perspective-api.py
@@ -0,0 +1,139 @@
+from googleapiclient import discovery
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import mmap
+import csv
+
+# API_KEY = ''
+
+# client = discovery.build(
+#   "commentanalyzer",
+#   "v1alpha1",
+#   developerKey=API_KEY,
+#   discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+#   static_discovery=False,
+# )
+
+# A function that takes as input a sentence prompt and returns an object with toxicity data
+def toxicity_rating(prompt, type):
+  analyze_request = {
+    'comment': { 'text': prompt },
+    'requestedAttributes': {type: {}}
+  }
+
+  response = client.comments().analyze(body=analyze_request).execute()
+  return json.dumps(response, indent=2)
+
+# A function that obtains toxicity for a list of sentences in a .txt file and 
+# puts the data in a file called toxicity.txt.  
+def retrieve_toxicity_data(filename):
+  f = open(filename, "r")
+  output = open("identity-attack.txt", "a")
+
+  for sentence in f:
+    print(sentence)
+    response = toxicity_rating(sentence, 'IDENTITY_ATTACK')
+    output.write(sentence)
+    output.write(response)
+  output.close()
+  f.close()
+
+
+def find_and_return_percentage(filename, target_string):
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+
+            found = False
+            for i, line in enumerate(lines):
+                if target_string in line:
+                    found = True
+                    print(f"Found '{target_string}' on line {i + 1}:")
+                    for j in range(i + 1, min(i + 21, len(lines))):
+                      index = lines[j].find("value")
+                      if index != -1: 
+                        output = float(lines[j][index+8: -2])
+                        if output != None:
+                          return output
+                        else: 
+                          return 0 
+
+            if not found:
+                print(f"String '{target_string}' not found in the file.")
+
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+
+#Given a specific prompt, plot the different attributes from perspective API 
+def plot_perspective_one_prompt (prompt): 
+  categories = ["toxicity", "severe_toxicity", "identity_attack", "threat", "profanity", "insult"]
+  toxicity = find_and_return_percentage("toxicity.txt", prompt)
+  severe_toxicity = find_and_return_percentage("severe-toxicity.txt", prompt)
+  identity_attack = find_and_return_percentage("identity-attack.txt", prompt)
+  threat = find_and_return_percentage("threat.txt", prompt)
+  profanity = find_and_return_percentage("profanity.txt", prompt)
+  insult = find_and_return_percentage("insult.txt", prompt)
+  
+  ratings = [toxicity, severe_toxicity, identity_attack, threat, profanity, insult]
+
+  fig, ax = plt.subplots()
+
+  bar_labels = ["toxicity", "severe_toxicity", "identity_attack", "threat", "profanity", "insult"]
+
+  ax.bar(categories, ratings, label=bar_labels)
+
+  ax.set_ylabel('Toxicity Categories')
+  ax.set_title('Toxicity: ' + prompt )
+
+  plt.show()
+
+# Plot all of the scores for each category from Perspective API
+def number_plot(): 
+  prompts = []
+  toxicity = []
+  severe_toxicity = []
+  identity_attack = []
+  sexually_explicit = []
+  threat = []
+  profanity = []
+  insult = []
+  f = open('prompts.txt', "r")
+
+  lines = f.readlines()
+  for prompt in lines:
+    prompts.append(prompt)
+    toxicity.append(find_and_return_percentage("toxicity.txt", prompt))
+    severe_toxicity.append(find_and_return_percentage("severe-toxicity.txt", prompt))
+    identity_attack.append(find_and_return_percentage("identity-attack.txt", prompt))
+    threat.append(find_and_return_percentage("threat.txt", prompt))
+    profanity.append(find_and_return_percentage("profanity.txt", prompt))
+    insult.append(find_and_return_percentage("insult.txt", prompt))
+    sexually_explicit.append(find_and_return_percentage("sexually-explicit.txt", prompt))
+  f.close()
+
+  xs = range(0, 223)
+  
+  plt.scatter(xs, toxicity, label = "toxicity")
+  plt.scatter(xs, severe_toxicity, label = "severe toxicity")
+  plt.scatter(xs, identity_attack, label = "identity attack")
+  plt.scatter(xs, threat, label = "threat")
+  plt.scatter(xs, profanity, label = "profanity")
+  plt.scatter(xs, insult, label = "insult")
+  plt.scatter(xs, sexually_explicit, label="sexually explicit")
+ 
+  plt.xlabel('Prompt Index')
+  plt.ylabel('Score')
+  plt.title('Toxicity Scores')
+  plt.legend()
+  plt.show()
+
+  with open('perspective-api-data.csv', 'w', newline='') as file:
+      writer = csv.writer(file)
+      row_list = []
+      for i in range(len(prompts)):
+        row= [prompts[i], toxicity[i], severe_toxicity[i], identity_attack[i], threat[i], profanity[i], insult[i], sexually_explicit[i]]
+        writer.writerow(row)
+
+
+number_plot()
---
+++ perspective-api/perspective-data-collection.py
@@ -0,0 +1,103 @@
+from googleapiclient import discovery
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import mmap
+from dotenv import load_dotenv
+import os
+import csv
+
+load_dotenv()
+
+API_KEY = os.environ["PERSPECTIVE_KEY"]
+
+client = discovery.build(
+  "commentanalyzer",
+  "v1alpha1",
+  developerKey=API_KEY,
+  discoveryServiceUrl="https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1",
+  static_discovery=False,
+)
+
+
+# A function that takes as input a sentence prompt and returns an object with toxicity data
+def toxicity_rating(prompt, type):
+  analyze_request = {
+    'comment': { 'text': prompt },
+    'requestedAttributes': {type: {}}
+  }
+
+  response = client.comments().analyze(body=analyze_request).execute()
+  return response
+
+def all_responses(filename):
+  prompts = []
+  f = open(filename, "r")
+  # Initialize Scores
+  toxicity_scores = []
+  severe_toxicity_scores = []
+  identity_attack_scores = []
+  threat_scores = []
+  profanity_scores = []
+  insult_scores = []
+  sexually_explicit_scores = []
+
+
+  lines = f.readlines()
+  for prompt in lines:
+      prompts.append(prompt)
+      toxicity_res = toxicity_rating(prompt, 'TOXICITY')
+      severe_toxicity_res = toxicity_rating(prompt, 'SEVERE_TOXICITY')
+      identity_attack_res = toxicity_rating(prompt, 'IDENTITY_ATTACK')
+      threat_res = toxicity_rating(prompt, 'THREAT')
+      profanity_res = toxicity_rating(prompt, 'PROFANITY')
+      insult_res = toxicity_rating(prompt, 'INSULT')
+      sexually_explicit_res = toxicity_rating(prompt, 'SEXUALLY_EXPLICIT')
+      # rating_response_dict = rating_response.model_dump()
+      toxicity = toxicity_res['attributeScores']['TOXICITY']['summaryScore']['value']
+      severe_toxicity = severe_toxicity_res['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
+      identity_attack = identity_attack_res['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
+      threat = threat_res['attributeScores']['THREAT']['summaryScore']['value']
+      profanity = profanity_res['attributeScores']['PROFANITY']['summaryScore']['value']
+      insult = insult_res['attributeScores']['INSULT']['summaryScore']['value']
+      sexually_explicit = sexually_explicit_res['attributeScores']['SEXUALLY_EXPLICIT']['summaryScore']['value']
+      
+      #Accumulate Scores 
+      toxicity_scores.append(toxicity)
+      severe_toxicity_scores.append(severe_toxicity)
+      identity_attack_scores.append(identity_attack)
+      threat_scores.append(threat)
+      profanity_scores.append(profanity)
+      insult_scores.append(insult)
+      sexually_explicit_scores.append(sexually_explicit)
+
+      
+  f.close()
+  return prompts, toxicity_scores, severe_toxicity_scores, identity_attack_scores, threat_scores, profanity_scores, insult_scores, sexually_explicit_scores
+
+
+# A function that obtains toxicity for a list of sentences in a .txt file and 
+# puts the data in a file called toxicity.txt.  
+def retrieve_toxicity_data(filename):
+  f = open(filename, "r")
+  output = open("identity-attack.txt", "a")
+
+  for sentence in f:
+    print(sentence)
+    response = toxicity_rating(sentence, 'IDENTITY_ATTACK')
+    output.write(sentence)
+    output.write(response)
+  output.close()
+  f.close()
+
+def make_csv(prompts, toxicity_scores, severe_toxicity_scores, identity_attack_scores, threat_scores, profanity_scores, insult_scores, sexually_explicit_scores):
+  with open('perspective-api-data.csv', 'w', newline='') as file:
+      writer = csv.writer(file)
+      row_list = []
+      for i in range(len(prompts)):
+        row= [prompts[i], prompts[i], toxicity_scores[i], severe_toxicity_scores[i], identity_attack_scores[i], threat_scores[i], profanity_scores[i], insult_scores[i], sexually_explicit_scores[i]]
+        writer.writerow(row)
+
+prompts, toxicity_scores, severe_toxicity_scores, identity_attack_scores, threat_scores, profanity_scores, insult_scores, sexually_explicit_scores = all_responses('../prompts.txt')
+
+make_csv(prompts, toxicity_scores, severe_toxicity_scores, identity_attack_scores, threat_scores, profanity_scores, insult_scores, sexually_explicit_scores)
