28cbbc19f4b324c60f7617f6d9bc19b7a27fd70c
as2759 <as2759@cornell.edu>
2024-04-21 16:56:54 -0400

    writing script to parse through survey data

--- README.md
+++
@@ -1,3 +0,0 @@
-# llm-ableism
-
-We are a group of researchers investigating ableism in LLMs.
---
+++ survey-data-cleanup.py
@@ -0,0 +1,67 @@
+import csv
+# Python program to delete a csv file 
+# csv file present in same directory 
+import os 
+
+# Delete output file if it alr exists
+file = 'survey-cleaned.csv'
+if(os.path.exists(file) and os.path.isfile(file)): 
+  os.remove(file) 
+  print("file deleted") 
+else: 
+  print("file not found") 
+
+def populated_list_indices(lst):
+  indices = []
+  count = 0 
+  for i in range(len(lst)): 
+    if lst[i]!= '': 
+      indices.append(i+1) 
+      count += 1
+  return count, indices
+
+with open('survey-data.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    with open('survey-cleaned.csv', 'w', newline='') as file:
+      writer = csv.writer(file)
+      counter = 0
+      headings = ['Prompt', 'Hurtful Scores', 'Toxic Scores', 'Ableist Scores', 'Implicit/Explicit', 'Explanations']
+      writer.writerow(headings)
+      cleaned_row = []
+      k = 0 
+
+      for row in reader:
+        if (counter % 5) == 0: 
+          
+          count, indices = populated_list_indices(row[2:])
+          if count == 0: 
+            cleaned_row.append([(row[0])[0:row[0].index("_")], None, None, None, None])
+          for sub in cleaned_row: 
+            writer.writerow(sub)
+          sub_row = list(0 for i in range(6))
+          sub_row[0] = (row[0])[0:row[0].index("_")]
+          cleaned_row = list(sub_row.copy() for i in range(count))
+
+        count, indices = populated_list_indices(row[2:])
+        
+        for k in range(count):
+          i = indices[k]
+
+          if ('How hurtful is this statement' in row[1]):
+            cleaned_row[k][1] = row[1+i]
+          if ('How toxic is this statement' in row[1]): 
+            cleaned_row[k][2] = row[1+i]
+          if ('How ableist is this statement' in row[1]): 
+            cleaned_row[k][3] = row[1+i]
+          if ('ableism implicit or explicit' in row[1]): 
+            cleaned_row[k][4] = row[1+i]
+          if ('Why is this comment ableist' in row[1]): 
+            cleaned_row[k][5] = row[1+i]
+
+        counter = counter + 1
+        
+
+
+
+
+        
